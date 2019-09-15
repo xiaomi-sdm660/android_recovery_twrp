6204c8e58f8e46c60e0b1ffae6d91bf4a329f4c2
nebrassy <nebras30@gmail.com>
2019-09-15 20:01:54 +0200

    initial import

---
+++ twrp.mk
@@ -0,0 +1,5 @@
+TWRP_PATH := device/twrp/jasmine_sprout
+
+TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
+TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip
+
