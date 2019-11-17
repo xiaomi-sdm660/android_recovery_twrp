582326be32565da52257b5cb135c1add873865b0
nebrassy <nebras30@gmail.com>
2019-11-17 02:45:35 +0100

    remove ramdisk-recovery.img
    * no longer needed
    
    Change-Id: If5221dde7c3e978488a386491d96fa2665d4780e

--- twrp.mk
+++ twrp.mk
@@ -1,5 +1,4 @@
 TWRP_PATH := device/twrp/jasmine_sprout
 
-TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
 TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip
 
